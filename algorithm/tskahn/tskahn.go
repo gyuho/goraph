bbe247907bec4c29cf427329a007a04d83cb1972
Gyu-Ho Lee <gyuho.cs@gmail.com>
2014-03-06 16:02:45 -0800

    Update trailing comma for godoc

--- algorithm/tskahn/tskahn.go
+++ algorithm/tskahn/tskahn.go
@@ -1,6 +1,6 @@
 // Package tskahn finds topological sort
 // based on algorithm by Arthur Kahn(1962).
-// Reference: http://dl.acm.org/citation.cfm?doid=368996.369025.
+// (Reference: http://dl.acm.org/citation.cfm?doid=368996.369025)
 package tskahn
 
 /*
