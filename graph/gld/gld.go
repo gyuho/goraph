d04595930619a4e0f518e90d1cf8968edb2b9d36
Gyu-Ho Lee <gyuho.cs@gmail.com>
2014-03-06 17:33:47 -0800

    Update gld with goson

--- graph/gld/gld.go
+++ graph/gld/gld.go
@@ -12,6 +12,8 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+
+	"github.com/gyuho/goson/jgd"
 )
 
 // Graph is a graph represented in adjacency list. Vertices and edges
@@ -345,4 +347,26 @@ func strToFloat(str string) float64 {
 		panic("Fail")
 	}
 	return f
+}
+
+// JSONGraph parses JSON file to a graph.
+func JSONGraph(filename, graph string) *Graph {
+	nodes := jgd.GetNodes(filename, graph)
+	gmap := jgd.MapGraph(filename, graph)
+	// map[string]map[string][]float64
+
+	g := NewGraph()
+	for _, srcID := range nodes {
+		// source vertex
+		src := g.CreateAndAddToGraph(srcID)
+		for dstID := range gmap[srcID] {
+			dst := g.CreateAndAddToGraph(dstID)
+			// This is not constructing the bi-directional edge automatically.
+			// We need to input bi-directional graph data.
+			for _, weight := range gmap[srcID][dstID] {
+				g.Connect(src, dst, weight)
+			}
+		}
+	}
+	return g
 }
