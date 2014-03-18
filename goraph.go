ab474e66bcd90bd7a392eadf44f6cab726c096f4
Gyu-Ho Lee <gyuho.cs@gmail.com>
2014-03-18 02:13:07 -0700

    Update

--- goraph.go
+++ goraph.go
@@ -1,7 +1,11 @@
 // goraph provides graph visualizing tools and graph algorithm implementations.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 func main() {
 	println()
@@ -25,6 +29,63 @@ Goraph provides:
 This is a personal project, non-committal on a timeline.
 Everything is still in heavy development.
 
+Type "help" for help.
+Type "Ctrl + c" or "q" to exit.`)
 
+	for {
+		fmt.Println(`
+[1] Visualize a Graph
+[2] Algorithm Visualization : Breadth First Search
+[3] Algorithm Visualization : Depth First Search
+[4] Algorithm Visualization : Shortest Path
+[5] Algorithm Visualization : Topological Sort
+[6] Algorithm Visualization : Minimum Spanning Tree
+[7] Algorithm Visualization : Network Flow
 `)
+		mm := make(map[string]string)
+		mm["1"] = "Visualize a Graph"
+		mm["2"] = "Algorithm Visualization : Breadth First Search"
+		mm["3"] = "Algorithm Visualization : Depth First Search"
+		mm["4"] = "Algorithm Visualization : Shortest Path"
+		mm["5"] = "Algorithm Visualization : Topological Sort"
+		mm["6"] = "Algorithm Visualization : Minimum Spanning Tree"
+		mm["7"] = "Algorithm Visualization : Network Flow"
+
+		fmt.Print("Select the menu: ")
+		var choice string
+		fmt.Scanf("%s", &choice)
+		if choice == "q" || choice == "Q" || choice == "quit" {
+			fmt.Print("Are you sure? [y/n]: ")
+			var qas string
+			fmt.Scanf("%s", &qas)
+			if strings.ToLower(qas) == "y" {
+				break
+			} else if strings.ToLower(qas) == "n" {
+				continue
+			}
+		} else {
+			fmt.Println("You've chosen \"" + mm[choice] + "\"")
+		}
+
+		switch choice {
+		case "1":
+			fmt.Println("1")
+		case "2":
+			fmt.Println("2")
+		case "3":
+			fmt.Println("3")
+		case "4":
+			fmt.Println("4")
+		case "5":
+			fmt.Println("5")
+		case "6":
+			fmt.Println("6")
+		case "7":
+			fmt.Println("7")
+		default:
+			fmt.Println("Done")
+		}
+
+		time.Sleep(time.Second)
+	}
 }
