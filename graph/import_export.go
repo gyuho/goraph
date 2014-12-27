29f7b2f58a205a1d96cb0c33043029ec30e16d68
Gyu-Ho Lee <gyuho.cs@gmail.com>
2014-12-27 10:42:18 -0800

    Add FromJSON

--- graph/import_export.go
+++ graph/import_export.go
@@ -1,8 +1,35 @@
 package graph
 
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
 // FromJSON constructs Data from JSON file.
-func FromJSON(fpath string) (*Data, error) {
-	return nil, nil
+func FromJSON(fpath string) (map[string]map[string]map[string]float64, error) {
+	file, err := os.Open(fpath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	jsonStream, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	// graphMap := make(map[string]map[string]map[string]float64)
+	graphMap := make(map[string]map[string]map[string]float64)
+	dec := json.NewDecoder(bytes.NewReader(jsonStream))
+	for {
+		if err := dec.Decode(&graphMap); err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+	}
+	return graphMap, nil
 }
 
 // ToJSON exports a graph Data to JSON file.
--- graph/import_export_test.go
+++ graph/import_export_test.go
@@ -3,7 +3,11 @@ package graph
 import "testing"
 
 func TestFromJSON(t *testing.T) {
-
+	rmap, err := FromJSON("../testdata/test_graph.json")
+	if err != nil {
+		t.Logf("%+v", rmap["graph_001"])
+		t.Errorf("Error: %+v", err)
+	}
 }
 
 func TestToJSON(t *testing.T) {
