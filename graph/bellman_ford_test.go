8d012f494de749a3256ced472ac088f1856fcabc
Gyu-Ho Lee <gyuho.cs@gmail.com>
2015-03-15 02:48:12 -0700

    Add Bellman-Ford algorithm

--- graph/bellman_ford_test.go
+++ graph/bellman_ford_test.go
@@ -1,1 +1,52 @@
 package graph
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestBellmanFord12(t *testing.T) {
+	file, err := os.Open("../testdata/data.json")
+	if err != nil {
+		t.Errorf("Error: %+v", err)
+	}
+	defer file.Close()
+	data, err := FromJSON(file, "test_graph_12")
+	if err != nil {
+		t.Errorf("Error: %+v", err)
+	}
+	pathSlice, mapToDistance, noNegCycle := data.BellmanFord(data.GetNodeByID("S"), data.GetNodeByID("T"))
+	ts := []string{}
+	for _, v := range pathSlice {
+		ts = append(ts, v.ID)
+	}
+	if strings.Join(ts, "-") != "S-A-C-B-T" {
+		t.Errorf("Expected the shortest path S-A-C-B-T but %s", strings.Join(ts, "-"))
+	}
+	if mapToDistance[data.GetNodeByID("T")] != -2.0 {
+		t.Errorf("Expected the shortest distance of -2.0 (from S to T) but %.2f", mapToDistance[data.GetNodeByID("T")])
+	}
+	if !noNegCycle {
+		t.Errorf("There should be no negative-weight cycle but found one with %v", noNegCycle)
+	}
+}
+
+func TestBellmanFord13(t *testing.T) {
+	file, err := os.Open("../testdata/data.json")
+	if err != nil {
+		t.Errorf("Error: %+v", err)
+	}
+	defer file.Close()
+	data, err := FromJSON(file, "test_graph_13")
+	if err != nil {
+		t.Errorf("Error: %+v", err)
+	}
+	pathSlice, mapToDistance, noNegCycle := data.BellmanFord(data.GetNodeByID("S"), data.GetNodeByID("T"))
+	if pathSlice != nil || mapToDistance != nil {
+		t.Errorf("Expected nil, nil but %v, %v", pathSlice, mapToDistance)
+	}
+	if noNegCycle {
+		t.Errorf("There should be a negative-weight cycle but found one with %v", noNegCycle)
+	}
+}
