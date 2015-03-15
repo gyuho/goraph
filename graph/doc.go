675444e4385e18bf0ab9e21556b668d21f11d2ed
Gyu-Ho Lee <gyuho.cs@gmail.com>
2015-03-15 01:45:13 -0700

    Update packagea description

--- graph/doc.go
+++ graph/doc.go
@@ -1,5 +1,4 @@
 // Package graph implements graph data structure with Go structs, pointers, and map.
-// (http://en.wikipedia.org/wiki/Adjacency_list)
 // This package does not allow duplicate(parallel) edges, referred to as a multigraph.
 // Connecting two nodes with duplicate edges overwrites the current weight value (not adding-up!).
 // http://www.cs.cmu.edu/afs/cs/academic/class/15210-s12/www/lectures/lecture18.pdf also replaces
