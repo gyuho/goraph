d013dd43936b5c082540e78d090a9afe2d397a9e
Gyu-Ho Lee <gyuho.cs@gmail.com>
2015-03-17 01:52:33 -0700

    Add Kruskal algorithm

---
+++ graph/edge.go
@@ -0,0 +1,23 @@
+package graph
+
+// Edge connects from Src to Dst with weight.
+type Edge struct {
+	Src    *Node
+	Dst    *Node
+	Weight float32
+}
+
+// GetEdges returns all edges of a graph.
+func (d *Data) GetEdges() []Edge {
+	rs := []Edge{}
+	for nd1 := range d.NodeMap {
+		for nd2, v := range nd1.WeightTo {
+			one := Edge{}
+			one.Src = nd1
+			one.Dst = nd2
+			one.Weight = v
+			rs = append(rs, one)
+		}
+	}
+	return rs
+}
