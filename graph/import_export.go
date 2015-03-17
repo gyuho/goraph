331c3d78e083a91f9b87aaf082bd86fdc6a615a7
Gyu-Ho Lee <gyuho.cs@gmail.com>
2015-03-17 02:09:41 -0700

    Update documentation

--- graph/import_export.go
+++ graph/import_export.go
@@ -21,7 +21,59 @@ func fromJSON(reader io.Reader) (map[string]map[string]map[string]float32, error
 	return graphMap, nil
 }
 
-// FromJSON creates a graph Data from JSON.
+// FromJSON creates a graph Data from JSON. Here's the sample JSON data:
+//
+//	{
+//	    "test_graph_01": {
+//	        "S": {
+//	            "A": 100,
+//	            "B": 14,
+//	            "C": 200
+//	        },
+//	        "A": {
+//	            "S": 15,
+//	            "B": 5,
+//	            "D": 20,
+//	            "T": 44
+//	        },
+//	        "B": {
+//	            "S": 14,
+//	            "A": 5,
+//	            "D": 30,
+//	            "E": 18
+//	        },
+//	        "C": {
+//	            "S": 9,
+//	            "E": 24
+//	        },
+//	        "D": {
+//	            "A": 20,
+//	            "B": 30,
+//	            "E": 2,
+//	            "F": 11,
+//	            "T": 16
+//	        },
+//	        "E": {
+//	            "B": 18,
+//	            "C": 24,
+//	            "D": 2,
+//	            "F": 6,
+//	            "T": 19
+//	        },
+//	        "F": {
+//	            "D": 11,
+//	            "E": 6,
+//	            "T": 6
+//	        },
+//	        "T": {
+//	            "A": 44,
+//	            "D": 16,
+//	            "F": 6,
+//	            "E": 19
+//	        }
+//	    },
+//	}
+//
 func FromJSON(reader io.Reader, graphName string) (*Data, error) {
 	gmap1, err := fromJSON(reader)
 	if err != nil {
--- graph/prim.go
+++ graph/prim.go
@@ -1,6 +1,26 @@
 package graph
 
-// Prim returns minimum spanning tree.
-func (d *Data) Prim(src, target *Node) {
+// Prim finds the minimum spanning tree with min-heap (priority queue).
+// Start a free from an arbitrary root Node r and grow the tree until
+// it spans all the Nodes in the graph. Maintain the heap with the minimum
+// weight value of edges.
+// (http://en.wikipedia.org/wiki/Prim%27s_algorithm)
+//
+//	for  each vertex  u ∈ G.V
+//		u.key = ∞
+//		u.π = NIL
+//
+//	r.key = 0
+//	Q = G.V
+//
+//	while  Q ≠ ø
+//		u = Extract-Min(Q)
+//		for each v ∈ G.Adj[u]
+//			if v ∈ Q  and  v.key > w(u, v)
+//				v.π = u
+//				v.key = w(u, v)
+//
+func (d *Data) Prim() map[Edge]bool {
 
+	return nil
 }
