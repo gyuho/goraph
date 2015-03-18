c1966d4d999f2b71b7000fc40249c81551f66a0f
Gyu-Ho Lee <gyuho.cs@gmail.com>
2015-03-18 00:01:09 -0700

    add adjacency list

---
+++ graph/adjacencylist/doc.go
@@ -0,0 +1,3 @@
+// Package adjacencylist implements a graph with adjacency list.
+// (http://en.wikipedia.org/wiki/Adjacency_list)
+package adjacencylist // import "github.com/gyuho/goraph/graph/adjacencylist"
