faaa1bd644d3c82129999f21b48d9e3bad65e1b2
Gyu-Ho Lee <gyuho.cs@gmail.com>
2015-03-24 08:49:22 -0700

    Add deletion (reference to Petar)

--- redblack/redblack_test.go
+++ redblack/redblack_test.go
@@ -61,3 +61,31 @@ func TestDeleteMin1(t *testing.T) {
 		t.Errorf("Unexpected %v", data)
 	}
 }
+
+func TestDeleteMax1(t *testing.T) {
+	root := NewNode(Str("S"))
+	data := New(root)
+	data.Insert(NewNode(Str("E")))
+	data.Insert(NewNode(Str("A")))
+	key := data.DeleteMax()
+	if fmt.Sprintf("%v", key) != "S" {
+		t.Errorf("Expected A but %v", key)
+	}
+	if fmt.Sprintf("%v", data) != "[[A(false)] E(true)]" {
+		t.Errorf("Unexpected %v", data)
+	}
+}
+
+func TestDelete1(t *testing.T) {
+	root := NewNode(Str("S"))
+	data := New(root)
+	data.Insert(NewNode(Str("E")))
+	data.Insert(NewNode(Str("A")))
+	key := data.Delete(Str("E"))
+	if fmt.Sprintf("%v", key) != "E" {
+		t.Errorf("Expected A but %v", key)
+	}
+	if fmt.Sprintf("%v", data) != "[[A(false)] S(true)]" {
+		t.Errorf("Unexpected %v", data)
+	}
+}
