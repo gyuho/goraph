33aa9d8cbe400cc5e413d8739dc0e8010745a2fa
Gyu-Ho Lee <gyuho.cs@gmail.com>
2015-07-17 13:22:41 -0700

    update docs

--- redblack/doc.go
+++ redblack/doc.go
@@ -1,10 +1,11 @@
 // Package redblack implements a Left-Leaning Red-Black tree
 // , which is a 2-3 balanced binary search tree:
 //	* Each edge(link between nodes) is either black or red.
+//	* Or every node is either black or red — easier to code.
 //	* Every path from root to null edge has the same number of black edges.
-//	* Red edges lean left.
-//	* Two red edges in a row are not allowed.
-//	* Only rotate red edges.
+//	* Red nodes(edges) lean left.
+//	* Two red nodes in a row are not allowed.
+//	* We only rotate red ones.
 //
 // Reference
 //	* http://www.cs.princeton.edu/~rs/talks/LLRB/08Penn.pdf
