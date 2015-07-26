55b7513c100413e7d395f6c1012ba6e111add5bc
Gyu-Ho Lee <gyuho.cs@gmail.com>
2015-07-26 10:59:12 -0700

    delete files

--- llrb/delete.go
+++ llrb/delete.go
@@ -1,5 +1,26 @@
 package llrb
 
+// Left and Right children must be present
+func moveRedToLeft(nd *Node) *Node {
+	flipColor(nd)
+	if isRed(nd.Right.Left) {
+		nd.Right = rotateToRight(nd.Right)
+		nd = rotateToLeft(nd)
+		flipColor(nd)
+	}
+	return nd
+}
+
+// Left and Right children must be present
+func moveRedToRight(nd *Node) *Node {
+	flipColor(nd)
+	if isRed(nd.Left.Left) {
+		nd = rotateToRight(nd)
+		flipColor(nd)
+	}
+	return nd
+}
+
 func fixUp(nd *Node) *Node {
 	if isRed(nd.Right) {
 		nd = rotateToLeft(nd)
