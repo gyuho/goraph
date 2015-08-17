28bb21ce13e9d23b58bc7dd96a9d75f8e2c9b3fd
Gyu-Ho Lee <gyuhox@gmail.com>
2015-08-16 20:40:05 -0700

    add more test cases for internal interface

--- graph/default_graph_test.go
+++ graph/default_graph_test.go
@@ -10,6 +10,15 @@ import (
 func TestNewDefaultGraphInterface(t *testing.T) {
 	g := NewDefaultGraph()
 	t.Log(g.String())
+	f, err := os.Open("testdata/graph.json")
+	if err != nil {
+		t.Error(err)
+	}
+	defer f.Close()
+	_, err = NewDefaultGraphFromJSON(f, "graph_00")
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 func TestNewDefaultGraphFromJSON(t *testing.T) {
