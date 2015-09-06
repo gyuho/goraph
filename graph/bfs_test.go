3195de19b73f2b8e10e8463b2ee4dd1ec3878f2f
Gyu-Ho Lee <gyuho.cs@gmail.com>
2015-09-06 09:40:34 -0700

    add BFS

---
+++ graph/bfs_test.go
@@ -0,0 +1,24 @@
+package graph
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestDefaultGraph_BFS(t *testing.T) {
+	f, err := os.Open("testdata/graph.json")
+	if err != nil {
+		t.Error(err)
+	}
+	defer f.Close()
+	g, err := NewDefaultGraphFromJSON(f, "graph_00")
+	if err != nil {
+		t.Error(err)
+	}
+	rs := BFS(g, "S")
+	fmt.Println(rs) // [S A B C D T E F]
+	if len(rs) != 8 {
+		t.Errorf("should be 8 vertices but %s", g)
+	}
+}
