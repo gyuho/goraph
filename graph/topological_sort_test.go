883840a8d4ca71f31f1337a8c6b28585cdd15f20
Gyu-Ho Lee <gyuho.cs@gmail.com>
2015-09-07 12:09:24 -0700

    add more tests for TopologicalSorT

--- graph/topological_sort_test.go
+++ graph/topological_sort_test.go
@@ -1,11 +1,63 @@
 package graph
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
 	"github.com/gyuho/goraph/graph/testdata"
 )
+
+func TestDefaultGraph_TopologicalSort_05(t *testing.T) {
+	f, err := os.Open("testdata/graph.json")
+	if err != nil {
+		t.Error(err)
+	}
+	defer f.Close()
+	g, err := NewDefaultGraphFromJSON(f, "graph_05")
+	if err != nil {
+		t.Error(err)
+	}
+	L, isDAG := TopologicalSort(g)
+	if isDAG != true {
+		t.Errorf("there is no directed cycle in the graph so isDAG should be true but %+v %+v", L, isDAG)
+	}
+	fmt.Println("graph_05:", L)
+}
+
+func TestDefaultGraph_TopologicalSort_06(t *testing.T) {
+	f, err := os.Open("testdata/graph.json")
+	if err != nil {
+		t.Error(err)
+	}
+	defer f.Close()
+	g, err := NewDefaultGraphFromJSON(f, "graph_06")
+	if err != nil {
+		t.Error(err)
+	}
+	L, isDAG := TopologicalSort(g)
+	if isDAG != true {
+		t.Errorf("there is no directed cycle in the graph so isDAG should be true but %+v %+v", L, isDAG)
+	}
+	fmt.Println("graph_06:", L)
+}
+
+func TestDefaultGraph_TopologicalSort_07(t *testing.T) {
+	f, err := os.Open("testdata/graph.json")
+	if err != nil {
+		t.Error(err)
+	}
+	defer f.Close()
+	g, err := NewDefaultGraphFromJSON(f, "graph_07")
+	if err != nil {
+		t.Error(err)
+	}
+	L, isDAG := TopologicalSort(g)
+	if isDAG != false {
+		t.Errorf("there is a directed cycle in the graph so isDAG should be false but %+v %+v", L, isDAG)
+	}
+	fmt.Println("graph_07:", L)
+}
 
 func TestDefaultGraph_TopologicalSort(t *testing.T) {
 	for _, graph := range testdata.GraphSlice {
