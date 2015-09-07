029395a57ada3e1a676956fe2422a2d913613d8c
Gyu-Ho Lee <gyuho.cs@gmail.com>
2015-09-07 15:59:52 -0700

    working on Tarjan algorithm (strongly connected components)

--- graph/strongly_connected_components.go
+++ graph/strongly_connected_components.go
@@ -1,1 +1,24 @@
 package graph
+
+// Tarjan finds the strongly connected components.
+// In the mathematics, a directed graph is "strongly connected"
+// if every vertex is reachable from every other node.
+// Therefore, a graph is strongly connected if there is a path
+// in each direction between each pair of node of a graph.
+// Then a pair of vertices u and v is strongly connected to each other
+// because there is a path in each direction.
+// "Strongly connected components" of an arbitrary graph
+// partition into sub-graphs that are themselves strongly connected.
+// That is, "strongly connected component" of a directed graph
+// is a sub-graph that is strongly connected.
+// Formally, "Strongly connected components" of a graph is a maximal
+// set of vertices C in G.V such that for all u, v ∈ C, there is a path
+// both from u to v, and from v to u.
+// (https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm)
+//
+//
+//
+func Tarjan(g Graph) [][]string {
+
+	return nil
+}
--- graph/strongly_connected_components_test.go
+++ graph/strongly_connected_components_test.go
@@ -1,1 +1,20 @@
 package graph
+
+import (
+	"os"
+	"testing"
+)
+
+func TestDefaultGraph_Tarjan_14(t *testing.T) {
+	f, err := os.Open("testdata/graph.json")
+	if err != nil {
+		t.Error(err)
+	}
+	defer f.Close()
+	g, err := NewDefaultGraphFromJSON(f, "graph_14")
+	if err != nil {
+		t.Error(err)
+	}
+	rs := Tarjan(g)
+	_ = rs
+}
